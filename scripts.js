2b38011190ac93125d9a9e7c562375fd3fb52cfa
Alexander <1eon3r@tut.by>
2022-05-24 22:55:47 +0300

    without errors in js file

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 let a1 = 5 % 3,
     a2 = 3 % 5,
     a3 = 5 + '3',
-    a4 = '5' – 3,
+    a4 = '5' - 3,
     a5 = 75 + 'кг',
     a6 = 785 * 653,
     a7 = 100 / 25,
@@ -10,13 +10,13 @@ let a1 = 5 % 3,
     a9 = 0 / 2,
     a10 = 89 / 0,
     a11 = 98 + 2,
-    a12 = 5 – 98;
+    a12 = 5 - 98,
     a13 = (8 + 56 * 4) / 5,
     a14 = (9 - 12) * 7 / (5 + 2),
     a15 = +"123",
     a16 = 1 || 0,
     a17 = false || true,
-    a18 = true > 0,
+    a18 = true > 0;
 console.log(a1, typeof a1);
 console.log(a2, typeof a2);
 console.log(a3, typeof a3);
@@ -34,7 +34,7 @@ console.log(a14, typeof a14);
 console.log(a15, typeof a15);
 console.log(a16, typeof a16);
 console.log(a17, typeof a17);
-console.log(a19, typeof a19);
+console.log(a18, typeof a18);
 
 
 
